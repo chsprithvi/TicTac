cf1a9254515f7dc8c3400da832861cfda8ce60ee
chsprithvi <chsprithvi@gmail.com>
2020-09-15 18:01:14 +0530

    [Siddhartha]Added:Toss function to select player or computer

--- TicTak.java
+++ TicTak.java
@@ -1,10 +1,16 @@
+import java.util.*;
 class TicTak{
     
     String[][] board= new String[3][3];
-    
+
+    //Function play to start game
+    public void play() {
+        display();
+        toss();
+        
+    }
     
     //function to display
-
 	public void display() {
 
 
@@ -19,7 +25,7 @@ class TicTak{
                     System.out.print(" ");    
                 }
 				else{
-                    System.out.print(board[i][j]+" ");
+                    System.out.print(this.board[i][j]+" ");
                 }
 				             
                 System.out.print("\t |");
@@ -28,13 +34,28 @@ class TicTak{
 			System.out.println("+--------+-------+-------+");
         }
         
+    }
+    
+    //function to check if player or computer chance
+    public void toss(){
+        Random rand = new Random();
+		// Generating a random number b/w 0 and 1
+		int checkOption = rand.nextInt(2);
+		//option 1 = PLAYER <> option 2 = COMPUTER 
+		if(checkOption == 1) {
+			System.out.println("Player Chance to Play");
+		}
+		else {
+			System.out.println("Computer Chance to Play");
+		}
 	}
+    
     
     public static void main(String[] args) {
         System.out.println(" %%%%%%% TicTakToe %%%%%%% ");
         //OBJ created
-        TicTak objTicTak=new TicTak();
-        objTicTak.display();
+        TicTak objTicTak = new TicTak();
+        objTicTak.play();
         
     }
 }
